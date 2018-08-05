f5f7793fa4b2e1e912c8b7a09f16a7b53a77ae04
Sabique A Khan <sabiquekhan@gmail.com>
2018-08-05 17:23:56 +0530

    Day 2, Lab 5 Completed
    1. Created a ViewModel class.
    2. Removed all the logics from the View.
    3. Used the ViewModel class to display the data for the Model class from the Controller to View.

--- LearnMVC/LearnMVC/Controllers/TestController.cs
+++ LearnMVC/LearnMVC/Controllers/TestController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using LearnMVC.Models;
+using LearnMVC.ViewModels;
 
 namespace LearnMVC.Controllers
 {
@@ -22,17 +23,34 @@ namespace LearnMVC.Controllers
             emp.LastName = "Khan";
             emp.Salary = 10000;
 
-            //Commented the code
-            //Uncomment the code if you need to pass the data to View using the ViewData
+            #region Code to pass the data to View using the ViewData
             //ViewData["Employee"] = emp;
-            //return View("MyView");
+            //return View("MyView"); 
+            #endregion
 
-            //Commented the code
-            //Passing the data to View using the ViewBag
+            #region Code to pass the data to View using the ViewBag
             //ViewBag.Employee = emp;
             //return View("MyView");
+            #endregion
 
-            return View("MyView", emp);
+            #region Code to pass the data to View using the Model
+            //return View("MyView", emp); 
+            #endregion
+
+            #region Code to pass the data to View using the ViewModel
+            EmployeeViewModel vmEmp = new EmployeeViewModel();
+            vmEmp.EmployeeName = emp.FirstName + " " + emp.LastName;
+            vmEmp.Salary = emp.Salary.ToString("C");
+
+            if (emp.Salary > 15000)
+                vmEmp.SalaryColor = "yellow";
+            else
+                vmEmp.SalaryColor = "green";
+
+            vmEmp.UserName = "Admin";
+
+            return View("MyView", vmEmp);
+            #endregion
         }
     }
 }
