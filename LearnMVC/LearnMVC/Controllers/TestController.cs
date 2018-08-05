4db9532981054c021535503938ff86edbfcda015
Sabique A Khan <sabiquekhan@gmail.com>
2018-08-05 14:48:45 +0530

    Day 1, Lab 1 Completed
    We added a controller and created a action method to return a simple string type.

---
+++ LearnMVC/LearnMVC/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearnMVC.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public string GetString()
+        {
+            return "Huraay! We have successfully run the ASP.Net MVC project.";
+        }
+    }
+}
