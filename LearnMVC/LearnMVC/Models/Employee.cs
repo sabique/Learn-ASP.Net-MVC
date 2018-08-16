1d8479194fccbdee127f76f5f3417f808c503490
Sabique A Khan <sabiquekhan@gmail.com>
2018-08-16 23:57:11 +0530

    Day 3, Lab 14 Completed
    1. Added the custom server side validation for the first name property.

--- LearnMVC/LearnMVC/Models/Employee.cs
+++ LearnMVC/LearnMVC/Models/Employee.cs
@@ -11,12 +11,33 @@ namespace LearnMVC.Models
         [Key]
         public int EmployeeId { get; set; }
 
-        [Required(ErrorMessage = "Enter First Name")]
+        //[Required(ErrorMessage = "Enter First Name")]
+        [FirstNameValidation]
         public string FirstName { get; set; }
 
         [StringLength(5, ErrorMessage ="Last Name length should not be greater than 5")]
         public string LastName { get; set; }
 
         public int Salary { get; set; }
+    }
+
+    public class FirstNameValidation:ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if(value == null)
+            {
+                return new ValidationResult("Please provide first name");
+            }
+            else
+            {
+                if(value.ToString().Contains("@"))
+                {
+                    return new ValidationResult("First name should not contain @");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
     }
 }
